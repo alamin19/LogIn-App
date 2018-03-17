9fd8630b48b24156aeac0ea12df0101f0035b39b
Al Amin <lisanalamin@gmail.com>
2018-03-17 11:14:02 -0400

    Created Forgot Password Screen, and added few UI elements

--- LogIn App/ForgetPasswordVC.swift
+++ LogIn App/ForgetPasswordVC.swift
@@ -1,15 +1,20 @@
 //
-//  SignUpVC.swift
+//  ForgetPasswordVC.swift
 //  LogIn App
 //
-//  Created by Saddam Al Amin on 3/16/18.
+//  Created by Saddam Al Amin on 3/17/18.
 //  Copyright © 2018 Saddam Al Amin. All rights reserved.
 //
 
 import UIKit
 
-class SignUpVC: UIViewControlle {
-
+class ForgetPasswordVC: UIViewController {
+    
+    // MARK:- Declaration -
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    // MARK:- ViewController Lifecycle Method -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,9 +26,16 @@ class SignUpVC: UIViewControlle {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - Action Methods -
+    
+    @IBAction func btnSubmitClicked(_ sender: UIButton) {
+        
+        
+    }
+    
 
     /*
-    // MARK: - Navigation
+    // MARK: - Navigation -
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -34,4 +46,7 @@ class SignUpVC: UIViewControlle {
 
 }
 
-
+extension ForgetPasswordVC: UITextFieldDelegate  {
+    
+    
+}
--- LogIn App/ViewController.swift
+++ LogIn App/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK:- ViewController Life Cycle -
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,6 +22,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK:- Action Buttons -
     
     @IBAction func btnSignUpClicked(_ sender: UIButton) {
         
@@ -28,6 +31,12 @@ class ViewController: UIViewController {
     
     @IBAction func btnLoginClicked(_ sender: UIButton) {
     }
+    
+    @IBAction func btnForgotPasswordClicked(_ sender: UIButton) {
+    }
+    
+    
+    // MARK:- Navigation -
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
